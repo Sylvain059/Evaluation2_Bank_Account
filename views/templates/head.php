354e7bd2158d6a80a77521a9c722037f20c76de6
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-09 14:27:26 +0100

    head.php // deleted file

--- views/templates/head.php
+++
@@ -1,9 +0,0 @@
-
-<head>
-	<meta charset="utf-8">
-	<title>Bank Account</title>
-	<!-- FONT LINK -->
-
-	<!-- CSS LINK -->
-	<link rel="stylesheet" type="text/css" href="../..assets/css/style.css">
-</head>
