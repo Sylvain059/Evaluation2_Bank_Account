870c4aa2c423ca499ce5c0aa9bcb7932e3ad080d
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-09 14:26:29 +0100

    indexVue.php // created file & include header.php & footer.php

--- views/indexVue.php
+++ views/indexVue.php
@@ -1,9 +1,9 @@
 <?php
-  include("template/header.php")
+  include("templates/header.php")
  ?>
 
 <p>This is the index view</p>
 
  <?php
-   include("template/footer.php")
+   include("templates/footer.php")
   ?>
