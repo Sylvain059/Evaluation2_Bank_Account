3668f4a2cb976f2aba1692ca51435790656975d5
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-09 15:10:31 +0100

    footer.php // add class & add paragraph tag

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,8 +1,10 @@
 <footer>
-  <p>cCopyright 2017, created by 2017</p>
+  	<div class="nav-wrapper">
+    	<p>Copyright 2017 created by Sylvain</p>
+   	</div>
 </footer>
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js""></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 </body>
 </html>
