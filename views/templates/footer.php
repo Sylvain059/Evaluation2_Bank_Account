71dde72fce8bd9069732f3c7003d46844cfdb3ea
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-09 14:24:04 +0100

    footer.php // created footer.php file

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,6 +1,8 @@
 <footer>
-  <p>A footer if you need one</p>
+  <p>cCopyright 2017, created by 2017</p>
 </footer>
 
+<script src="https://code.jquery.com/jquery-3.2.1.min.js""></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
 </body>
 </html>
