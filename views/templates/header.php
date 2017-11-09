f9a1f786499ba829e208206f6182c6b3b65348a2
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-09 13:57:40 +0100

    header.php // created file & add meta, title & css link

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Your site title</title>
-  </head>
-  <body>
-    <header>
-      <h1>Main title in the header</h1>
-    </header>
+
+<head>
+	<meta charset="utf-8">
+	<title>Bank Account</title>
+	<!-- FONT LINK -->
+
+	<!-- CSS LINK -->
+	<link rel="stylesheet" type="text/css" href="../..assets/css/style.css">
+</head>
