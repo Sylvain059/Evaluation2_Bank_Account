bed3ab1e7cde5de9b8b33e26cd4b2e0872cfaf83
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-09 14:25:06 +0100

    header.php // created header.php & add head & header tag

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,9 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bank Account</title>
 
-<head>
-	<meta charset="utf-8">
-	<title>Bank Account</title>
-	<!-- FONT LINK -->
-
-	<!-- CSS LINK -->
-	<link rel="stylesheet" type="text/css" href="../..assets/css/style.css">
-</head>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+  </head>
+  <body>
+    <header>
+      <h1>Bank Account</h1>
+      <p><a href="../home.php">Home</a></p>
+    </header>
