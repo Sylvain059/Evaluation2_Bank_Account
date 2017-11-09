2a78870cc11328a46bef85e879224be674d2203d
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-09 13:59:23 +0100

    change file name

---
+++ views/templates/head.php
@@ -0,0 +1,9 @@
+
+<head>
+	<meta charset="utf-8">
+	<title>Bank Account</title>
+	<!-- FONT LINK -->
+
+	<!-- CSS LINK -->
+	<link rel="stylesheet" type="text/css" href="../..assets/css/style.css">
+</head>
