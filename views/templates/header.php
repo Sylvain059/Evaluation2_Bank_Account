40068ef9cb34ce6580797b45b6d25c0e1a5f643d
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-09 15:06:26 +0100

    header.php // add materialize framework & add navbar

--- views/templates/header.php
+++ views/templates/header.php
@@ -3,12 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title>Bank Account</title>
-
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
   </head>
   <body>
-    <header>
-      <h1>Bank Account</h1>
-      <p><a href="../home.php">Home</a></p>
-    </header>
+  	<header>
+  		<nav>
+    		<div class="nav-wrapper">
+      			<a href="indexVue.php" class="brand-logo center">Bank Account</a>
+      			<ul id="nav-mobile" class="right">
+       			 <li><a href="indexVue.php">Home</a></li>
+      			</ul>
+    		</div>
+ 		 </nav>
+  	</header>
