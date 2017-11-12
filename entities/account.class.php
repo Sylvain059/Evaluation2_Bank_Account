4c57ab0217129e429c628ebbadee691dec398586
Sylvain059 <sylvain.sculier88@gmail.com>
2017-11-12 18:56:17 +0100

    account.class.php // add function construct

--- entities/account.class.php
+++ entities/account.class.php
@@ -7,4 +7,12 @@ class Account
     private $lastName;
     private $firstName;
     private $accountNumber;
+
+    //create function construct
+    public function __construct($lastName, $firstName, $accountNumber)
+    {
+        $this->lastName = $lastName;
+        $this->firstName = $firstName;
+        $this->accountNumber = $accountNumber;
+    }
 }
